a6e938641ec6d60e176bd844f8a42805ccc290f3
Ege Ulgen <egeulgen@gmail.com>
2017-12-29 11:57:37 +0300

    Add options to use different PPIs

---
+++ R/pin_path.R
@@ -0,0 +1,20 @@
+#' Return Path to Given Protein Interaction Network (PIN)
+#'
+#' @param pin_name Name of the chosen PIN. Must be one of c("Biogrid", "STRING",
+#'   "GeneMania", "BioPlex"). Defaults to "GeneMania".
+#'
+#' @return A character value that contains the path to chosen PIN.
+#'
+#' @export
+#'
+#' @examples
+#' pin_path <- return_pin_path("Biogrid")
+return_pin_path <- function(pin_name = "GeneMania") {
+  if (!pin_name %in% c("Biogrid", "STRING", "GeneMania", "BioPlex"))
+    stop(paste0("The chosen PIN must be one of:\n",
+                "Biogrid, STRING, GeneMania, BioPlex"))
+
+  path <- normalizePath(system.file(paste0("data/", pin_name, ".sif"),
+                            package = "pathfindr"))
+  return(path)
+}
