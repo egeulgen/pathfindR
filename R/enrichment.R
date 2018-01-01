8ec21dd0360399230de81abc80f59d20659d877d
Ege Ulgen <egeulgen@gmail.com>
2018-01-01 16:29:52 +0300

    change enrichment

--- R/enrichment.R
+++ R/enrichment.R
@@ -8,6 +8,8 @@
 #' @param pathways_list List that contains pathway descriptions for KEGG pathway
 #'   IDs. Names of this list are the KEGG IDs.
 #' @param adj_method correction method to be used for adjusting p-values.
+#' @param enrichment_threshold adjusted p value threshold used when filtering
+#'   pathway enrichment result
 #'
 #' @return A data frame that contains enrichment results.
 #' @export
@@ -17,43 +19,47 @@
 #' @examples
 #' enrichment(genes_by_pathway, genes_of_interest, pathways_list)
 enrichment <- function(genes_by_pathway, genes_of_interest,
-                       pathways_list, adj_method = "bonferroni") {
-  hyperg <- Category:::.doHyperGInternal
+                       pathways_list, adj_method = "bonferroni",
+                       enrichment_threshold = 1e-4) {
   hyperg_test <- function(pw_genes, chosen_genes, all_genes, over = TRUE) {
     pw_genes_selected <- length(intersect(chosen_genes, pw_genes))
-    pw_genes_in_pool <- length(pw_genes)
+    pw_genes_in_pool <- sum(pw_genes %in% all_genes)
     tol_genes_n_pool <- length(all_genes)
     non_pw_genes_in_pool <- tol_genes_n_pool - pw_genes_in_pool
     num_selected_genes <- length(chosen_genes)
-    hyperg(pw_genes_in_pool, non_pw_genes_in_pool,
-           num_selected_genes, pw_genes_selected, over)
+
+    phyper(pw_genes_selected - 1, pw_genes_in_pool,
+           non_pw_genes_in_pool, num_selected_genes, lower.tail = F)
   }
 
   all_genes <- AnnotationDbi::keys(org.Hs.eg.db:::org.Hs.eg.db, "SYMBOL")
 
-  enrichment_res <- t(sapply(genes_by_pathway, hyperg_test,
-                             genes_of_interest, all_genes))
+  enrichment_res <- sapply(genes_by_pathway, hyperg_test,
+                             genes_of_interest, all_genes)
   enrichment_res <- as.data.frame(enrichment_res)
+  colnames(enrichment_res) <- "p_value"
 
-  enrichment_res$p <- unlist(enrichment_res$p)
-  enrichment_res$odds <- unlist(enrichment_res$odds)
-  enrichment_res$expected <- unlist(enrichment_res$expected)
-
-  enrichment_res <- enrichment_res[order(enrichment_res$p), ]
+  enrichment_res <- enrichment_res[order(enrichment_res$p_value), , drop = F]
   enrichment_res$adj_p <- p.adjust(enrichment_res$p, method = adj_method)
 
-  ## pathway IDs
-  enrichment_res$ID <- rownames(enrichment_res)
+  if (all(enrichment_res$adj_p > enrichment_threshold))
+    return(NULL)
+  else {
+    enrichment_res <- enrichment_res[enrichment_res$adj_p <= enrichment_threshold,]
 
-  ## Pathway descriptions
-  idx <- match(paste0("path:", enrichment_res$ID), names(pathways_list))
-  enrichment_res$Pathway <- pathways_list[idx]
-  enrichment_res$Pathway <- sub(" - Homo sapiens \\(human\\)", "",
-                                enrichment_res$Pathway)
+    ## pathway IDs
+    enrichment_res$ID <- rownames(enrichment_res)
 
-  ## reorder columns
-  remaining <- setdiff(colnames(enrichment_res), c("ID", "Pathway"))
-  enrichment_res <- enrichment_res[, c("ID", "Pathway", remaining)]
+    ## Pathway descriptions
+    idx <- match(paste0("path:", enrichment_res$ID), names(pathways_list))
+    enrichment_res$Pathway <- pathways_list[idx]
+    enrichment_res$Pathway <- sub(" - Homo sapiens \\(human\\)", "",
+                                  enrichment_res$Pathway)
 
-  return(enrichment_res)
+    ## reorder columns
+    remaining <- setdiff(colnames(enrichment_res), c("ID", "Pathway"))
+    enrichment_res <- enrichment_res[, c("ID", "Pathway", remaining)]
+
+    return(enrichment_res)
+  }
 }
