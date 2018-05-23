bb6f8a43ede3caa267cbf19820ecf3232b56f06a
Ege Ulgen <egeulgen@gmail.com>
2018-05-23 18:22:36 +0300

    add grouping by cluster

--- R/wrapper.R
+++ R/wrapper.R
@@ -340,20 +340,24 @@ run_pathfindR <- function(input, p_val_threshold = 5e-2,
 #'   \item{lowest_p}{the lowest adjusted-p value of the given pathway over all iterations}
 #'   \item{Up_regulated}{the up-regulated genes in the input involved in the given pathway, comma-separated}
 #'   \item{Down_regulated}{the down-regulated genes in the input involved in the given pathway, comma-separated}
+#'   \item{Cluster(OPTIONAL)}{the cluster to which the pathway is assigned}
 #' }
+#' @param plot_by_cluster boolean value indicating whether or not to group the
+#' pathways by cluster (works if "Cluster" is a column of `result_df`).
 #'
 #' @return a `ggplot2` object containing the bubble chart. The x-axis corresponds to
 #' fold enrichment values while the y-axis indicates the enriched pathways. Size of
 #' the bubble indicates the number of DEGs in the given pathway. Color indicates
 #' the -log10(lowest-p) value. The closer the color is to red, the more significant
-#' the enrichment is.
+#' the enrichment is. Optionally, if "Cluster" is a column of `result_df` and
+#' plot_by_cluster == TRUE, the pathways are grouped by clusters.
 #'
 #' @import ggplot2
 #' @export
 #'
 #' @examples
 #' g <- enrichment_chart(RA_output)
-enrichment_chart <- function(result_df) {
+enrichment_chart <- function(result_df, plot_by_cluster = FALSE) {
   # sort by lowest adj.p
   result_df <- result_df[order(result_df$lowest_p), ]
 
@@ -372,6 +376,13 @@ enrichment_chart <- function(result_df) {
   g <- g + ggplot2::xlab("Fold Enrichment") + ggplot2::ylab('')
   g <- g + ggplot2::labs(size = "# of DEGs", color = "-log10(lowest-p)")
   g <- g + ggplot2::scale_color_continuous(low = "#f5efef", high = "red")
+
+  if (plot_by_cluster & "Cluster" %in% colnames(result_df)) {
+    g <- g + ggplot2::facet_grid(result_df$Cluster~., scales = "free_y", drop = TRUE)
+  } else if (plot_by_cluster) {
+    warning("For plotting by cluster, there must a column named `Cluster` in the input data frame!")
+  }
+
   return(g)
 }
 
