09ee3bdc61ec21b352404e0105740ce3c8226784
Ege Ulgen <egeulgen@gmail.com>
2018-11-11 17:36:46 +0300

    add enrichment_analyses

--- R/enrichment_functions.R
+++ R/enrichment_functions.R
@@ -11,7 +11,6 @@
 #' hypergeometric distribution is assumed and the appropriate p value
 #' (the value under the right tail) is calculated and returned.
 #'
-#'
 #' @export
 #'
 #' @examples
@@ -112,4 +111,94 @@ enrichment <- function(genes_by_pathway, genes_of_interest,
 
     return(enrichment_res)
   }
+}
+
+#' Perform Enrichment Analyses on the Input Subnetworks
+#'
+#' @param snws a list of subnetwork genes (i.e., vectors of genes for each subnetwork)
+#' @param gene_sets the gene sets used for enrichment analysis. Available gene sets
+#'  are KEGG, Reactome, BioCarta, GO-All, GO-BP, GO-CC, GO-MF or Custom. If "Custom", the arguments
+#'  custom_genes and custom pathways must be specified. (Default = "KEGG")
+#' @param custom_genes a list containing the genes involved in each custom pathway. Each element
+#' is a vector of gene symbols located in the given pathway. Names correspond to
+#' the ID of the pathway.
+#' @param custom_pathways A list containing the descriptions for each custom pathway. Names of the
+#' list correspond to the ID of the pathway.
+#' @param pin_path path to the Protein Interaction Network (PIN) file used in
+#'   the analysis
+#' @param input_genes vector of input gene symbols (that were used during active subnetwork genes)
+#' @param adj_method correction method to be used for adjusting p-values of
+#'  pathway enrichment results (Default: 'bonferroni', see ?p.adjust)
+#' @param enrichment_threshold threshold used when filtering individual iterations' pathway
+#'  enrichment results
+#' @param list_active_snw_genes boolean value indicating whether or not to report
+#' the non-DEG active subnetwork genes for the active subnetwork which was enriched for
+#' the given pathway with the lowest p value (default = FALSE)
+#'
+#'
+#' @return a dataframe of combined enrichment results. Columns are: \describe{
+#'   \item{ID}{KEGG ID of the enriched pathway}
+#'   \item{Pathway}{Description of the enriched pathway}
+#'   \item{Fold_Enrichment}{Fold enrichment value for the enriched pathway}
+#'   \item{p_value}{p value of enrichment}
+#'   \item{adj_p}{adjusted p value of enrichment}
+#'   \item{non_DEG_Active_Snw_Genes (OPTIONAL)}{the non-DEG active subnetwork genes, comma-separated}
+#' }
+#'
+#' @export
+#'
+#' @seealso \code{\link{enrichment}} for the enrichment analysis for a single gene set
+#'
+#' @examples
+#' \dontrun{
+#' enrichment_analyses(snws, input_genes = my_input$GENE, gene_sets = "KEGG", pin_path = "path/to/PIN")
+#' }
+#'
+enrichment_analyses <- function(snws, input_genes,
+                                gene_sets = "KEGG", custom_genes = NULL, custom_pathways = NULL,
+                                pin_path,
+                                adj_method = "bonferroni", enrichment_threshold = 5e-2,
+                                list_active_snw_genes = FALSE) {
+  ############ Load Gene Set Data
+  gene_sets_df <- data.frame("Gene Set Name" = c("KEGG", "Reactome", "BioCarta",
+                                                 "GO-All", "GO-BP", "GO-CC", "GO-MF"),
+                             "genes_by_pathway" = c("kegg_genes", "reactome_genes", "biocarta_genes",
+                                                    "go_all_genes", "go_bp_genes", "go_cc_genes", "go_mf_genes"),
+                             "pathways_list" = c("kegg_pathways", "reactome_pathways", "biocarta_pathways",
+                                                 "go_all_pathways", "go_bp_pathways", "go_cc_pathways", "go_mf_pathways"))
+
+  if (gene_sets %in% gene_sets_df$Gene.Set.Name) {
+    idx <- which(gene_sets_df$Gene.Set.Name == gene_sets)
+    genes_name <- gene_sets_df$genes_by_pathway[idx]
+    pws_name <-  gene_sets_df$pathways_list[idx]
+
+    genes_by_pathway <- base::eval(parse(text = paste0("pathfindR::", genes_name)))
+    pathways_list <- base::eval(parse(text = paste0("pathfindR::", pws_name)))
+
+  } else if (gene_sets == "Custom") {
+    genes_by_pathway <- custom_genes
+    pathways_list <- custom_pathways
+  }
+
+  ############ Enrichment per subnetwork
+  enrichment_res <- lapply(snws, function(x)
+    pathfindR::enrichment(genes_by_pathway, x, pathways_list,
+                          adj_method, enrichment_threshold,
+                          pin_path, DEG_vec = input_genes))
+
+  ############ Combine Enrichments Results for All Subnetworks
+  enrichment_res <- Reduce(rbind, enrichment_res)
+
+  ############ Process if non-empty
+  if (!is.null(enrichment_res)) {
+    ## delete non_DEG_Active_Snw_Genes if list_active_snw_genes == FALSE
+    if (!list_active_snw_genes)
+      enrichment_res$non_DEG_Active_Snw_Genes <- NULL
+
+    ## keep lowest p for each pathway
+    idx <- order(enrichment_res$adj_p)
+    enrichment_res <- enrichment_res[idx, ]
+    enrichment_res <- enrichment_res[!duplicated(enrichment_res$ID), ]
+  }
+  return(enrichment_res)
 }
