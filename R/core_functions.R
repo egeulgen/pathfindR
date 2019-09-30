5d4ef55ac883f57ff41b0dbf84c6508c11bd939a
Ege Ulgen <egeulgen@gmail.com>
2019-10-01 01:02:19 +0300

    add `fetch_gene_set`

--- R/core_functions.R
+++ R/core_functions.R
@@ -361,6 +361,90 @@ run_pathfindR <- function(input, p_val_threshold = 5e-2,
   return(final_res)
 }
 
+#' Fetch Gene Set Objects
+#'
+#' Function for obtaining the gene sets per term and the term descriptions to
+#' be used for enrichment analysis.
+#'
+#' @param gene_sets Name of the gene sets to be used for enrichment analysis.
+#'  Available gene sets are "KEGG", "Reactome", "BioCarta", "GO-All",
+#'  "GO-BP", "GO-CC", "GO-MF" or "Custom".
+#'  If "Custom", the arguments \code{custom_genes} and \code{custom_descriptions}
+#'  must be specified. (Default = "KEGG")
+#' @param custom_genes a list containing the genes involved in each custom
+#'  term. Each element is a vector of gene symbols located in the given custom
+#'  term. Names should correspond to the IDs of the custom terms.
+#' @param custom_descriptions A vector containing the descriptions for each
+#'  custom  term. Names of the vector should correspond to the IDs of the custom
+#'  terms.
+#'
+#' @return a list containing 2 elements \describe{
+#'   \item{genes_by_term}{list of vectors of genes contained in each term}
+#'   \item{term_descriptions}{vector of descriptions per each term}
+#' }
+#'
+#' @examples
+#' KEGG_gset <- pathfindR:::fetch_gene_set()
+#' GO_MF_gset <- pathfindR:::fetch_gene_set("GO-MF")
+fetch_gene_set <- function(gene_sets = "KEGG",
+                           custom_genes = NULL,
+                           custom_descriptions = NULL) {
+
+  # parse gene_sets argument
+  all_gs_opts <- c("KEGG", "Reactome", "BioCarta",
+                   "GO-All", "GO-BP", "GO-CC", "GO-MF",
+                   "Custom")
+  gene_sets <- match.arg(gene_sets, all_gs_opts)
+
+  ### Custom Gene Sets
+  if (gene_sets == "Custom") {
+    if (is.null(custom_genes) | is.null(custom_descriptions)) {
+      stop("`custom_genes` and `custom_descriptions` must be provided if `gene_sets = \"Custom\"`")
+    }
+    return(list(genes_by_term = custom_genes,
+                term_descriptions = custom_descriptions))
+  }
+
+  ### Built-in Gene Sets
+  ## GO gene sets
+  if (grepl("^GO", gene_sets)) {
+    genes_by_term <- pathfindR::go_all_genes
+
+    GO_df <- pathfindR:::GO_all_terms_df
+    term_descriptions <- GO_df$GO_term
+    names(term_descriptions) <- GO_df$GO_ID
+
+    if (gene_sets == "GO-BP") {
+      tmp <- GO_df$GO_ID[GO_df$Category == "Process"]
+      genes_by_term <- genes_by_term[tmp]
+      term_descriptions <- term_descriptions[tmp]
+    } else if (gene_sets == "GO-CC") {
+      tmp <- GO_df$GO_ID[GO_df$Category == "Component"]
+      genes_by_term <- genes_by_term[tmp]
+      term_descriptions <- term_descriptions[tmp]
+    } else if (gene_sets == "GO-MF") {
+      tmp <- GO_df$GO_ID[GO_df$Category == "Function"]
+      genes_by_term <- genes_by_term[tmp]
+      term_descriptions <- term_descriptions[tmp]
+    }
+
+    ## non-GO (KEGG, Reactome, BioCarta)
+  } else {
+    if (gene_sets == "KEGG") {
+      genes_by_term <- pathfindR::kegg_genes
+      term_descriptions <- pathfindR::kegg_descriptions
+    } else if (gene_sets == "Reactome") {
+      genes_by_term <- pathfindR::reactome_genes
+      term_descriptions <- pathfindR::reactome_descriptions
+    } else {
+      genes_by_term <- pathfindR::biocarta_genes
+      term_descriptions <- pathfindR::biocarta_descriptions
+    }
+  }
+  return(list(genes_by_term = genes_by_term,
+              term_descriptions = term_descriptions))
+}
+
 #' Return The Path to Given Protein-Protein Interaction Network (PIN)
 #'
 #' This function returns the path/to/PIN.sif. While the default PINs are
