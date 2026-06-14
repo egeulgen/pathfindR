2998257ecb5e0e43137e4101e28f967428a047e8
Ege Ulgen <egeulgen@gmail.com>
2026-06-14 10:08:51 +0100

    add example

--- R/active_snw_search_main.R
+++ R/active_snw_search_main.R
@@ -21,6 +21,48 @@
 #' @seealso \code{\link{get_active_subnetworks}} which builds \code{network},
 #'   \code{score_context} and \code{params} and calls this function.
 #'
+#' @examples
+#' \dontrun{
+#' # Write a minimal SIF file
+#' sif <- data.frame(
+#'   source = c("A", "A", "B", "C", "D"),
+#'   type   = "interacts",
+#'   target = c("B", "C", "C", "D", "E")
+#' )
+#' sif_path <- tempfile(fileext = ".sif")
+#' write.table(sif, sif_path,
+#'   sep = "\t", row.names = FALSE, col.names = FALSE,
+#'   quote = FALSE
+#' )
+#'
+#' experiment <- data.frame(
+#'   gene   = c("A", "B", "C", "D", "E"),
+#'   pvalue = c(0.001, 0.002, 0.001, 0.5, 0.6)
+#' )
+#'
+#' params <- list(
+#'   start_with_all_positives = FALSE,
+#'   gene_init_prob           = 0.1,
+#'   p_for_nonsignificant     = 0.5,
+#'   sa_initial_temp          = 1.0,
+#'   sa_final_temp            = 0.01,
+#'   sa_iterations            = 10000L,
+#'   ga_population_size       = 400L,
+#'   ga_iterations            = 200L,
+#'   ga_crossover_rate        = 1,
+#'   ga_mutation_rate         = 0,
+#'   gr_max_depth             = 1L,
+#'   gr_search_depth          = 1L,
+#'   gr_overlap_threshold     = 0.5,
+#'   gr_subnetwork_num        = 1000,
+#'   seed                     = 1234L
+#' )
+#'
+#' network <- .build_network(sif_path)
+#' sc <- .build_score_context(network, experiment, params)
+#' snws <- active_subnetwork_search(network, sc, method = "GR", params = params)
+#' }
+#'
 #' @export
 active_subnetwork_search <- function(network, score_context, method = c("GR", "SA", "GA"),
                                      params, verbose = FALSE) {
