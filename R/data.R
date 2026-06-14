92f0aa953a3605264a3fc5d0326a9682e1c1be6b
Ege Ulgen <egeulgen@gmail.com>
2026-06-14 09:03:31 +0100

    make data external

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Example Unfiltered Active Subnetworks
+#'
+#' List containing unfiltered active subnetworks returned by
+#' \code{\link{active_subnetwork_search}}
+#'
+#' @format A list with 1000 elements. Each containing a named list:
+#' \describe{
+#'   \item{nodes}{actove subnetwork nodes}
+#'   \item{score}{score of the active subnetwork}
+#' }
+"example_unfiltered_snws"
